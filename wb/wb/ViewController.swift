558f412bcdfd0017c3c3069d389d66f714ba2c83
YLing-Hou <hyl1324838552@163.com>
2026-03-30 22:50:06 +0800

    Revert "Task/add list (#1)"
    This reverts commit 98dcab5d4f12eff54361b369d8a158b9d10d108a.

--- wb/wb/ViewController.swift
+++ wb/wb/ViewController.swift
@@ -9,61 +9,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    private lazy var tableView: UITableView = {
-        let tableView = UITableView(frame: .zero, style: .plain)
-        tableView.translatesAutoresizingMaskIntoConstraints = false
-        tableView.dataSource = self
-        tableView.delegate = self
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
-        return tableView
-    }()
-    
-    
-    
-    
-    
-    
-    
-    
-    
-
-    private let dataList: [String] = (1...30).map { "\($0)" }
-
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        view.addSubview(tableView)
-        NSLayoutConstraint.activate([
-            tableView.topAnchor.constraint(equalTo: view.topAnchor),
-            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
-            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
-        ])
+        self.view.backgroundColor = .red
+        // Do any additional setup after loading the view.
     }
+
+
 }
 
-extension ViewController: UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return dataList.count
-    }
-
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        cell.textLabel?.text = dataList[indexPath.row]
-        return cell
-    }
-}
-
-extension ViewController: UITableViewDelegate {
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 60
-    }
-
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        tableView.deselectRow(at: indexPath, animated: true)
-        let text = dataList[indexPath.row]
-        let alert = UIAlertController(title: text, message: nil, preferredStyle: .alert)
-        alert.addAction(UIAlertAction(title: "确定", style: .default))
-        present(alert, animated: true)
-    }
-}
